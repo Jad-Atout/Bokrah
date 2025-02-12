6adf2741389fa2e2e3d346073e5fb620d0abc7d9
Jad-Atout <JadAtout.2003@outlook.sa>
2025-02-12 13:48:34 +0200

    "♻️ refactor: update createCalendarEvent to accept services argument
    Updated the `createCalendarEvent` function to take `services` as a separate
    argument. This improves separation of

--- Backend/AMS/src/utils/google/eventCRUD/createEvent.js
+++ Backend/AMS/src/utils/google/eventCRUD/createEvent.js
@@ -1,10 +1,10 @@
 import {google} from "googleapis";
 
-const createCalendarEvent = async (auth, appointment,calendarId) => {
+const createCalendarEvent = async (auth, appointment,services,calendarId) => {
     const calendar = google.calendar({ version: 'v3', auth });
     const event = {
         summary: `Appointment with ${appointment.customerId}`,
-        description: `Services: ${appointment.services.join(', ')}`,
+        description: `Services: ${services.join(', ')}`,
         start: { dateTime: new Date(appointment.startTime).toISOString(), timeZone: 'UTC' },
         end: { dateTime: new Date(appointment.endTime).toISOString(), timeZone: 'UTC' },
     };
