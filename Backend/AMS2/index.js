8229c291b83fe44926ff69bfdd93406284c3e432
Jad-Atout <JadAtout.2003@outlook.sa>
2025-02-18 23:51:39 +0200

    Re-add AMS2 after fixing .gitignore

---
+++ Backend/AMS2/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import initApp from "./src/app.js";
+import * as dotenv from "dotenv";
+dotenv.config();
+ const app = express();
+initApp(app,express)
+app.listen(process.env.PORT||10000, () => {
+    console.log("Server running on port: " + process.env.PORT);
+})
+export { app }
