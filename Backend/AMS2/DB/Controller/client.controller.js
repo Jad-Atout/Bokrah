59aa4d9dea3a06ad168c6944b425a9f3e13b509d
razan0r <razanhamad815@gmail.com>
2025-02-23 00:26:29 +0300

    fixing merge

--- Backend/AMS2/DB/Controller/client.controller.js
+++
@@ -1,8 +0,0 @@
-import clientModel from "../models/client.js"
-
-export const createClient = async (clientData)=>{
-    return clientModel(clientData)
-}
-export const findClientByUserId = async (id)=>{
-    return await clientModel.findOne({userId: id})
-}
