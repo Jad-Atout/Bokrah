5e9dc4ae1670c67fcc8006500f2ed52d3209a144
razan0r <razanhamad815@gmail.com>
2025-03-16 01:58:23 +0300

    🔥 fixing in google auth

--- Backend/AMS2/src/modules/client/client.router.js
+++ Backend/AMS2/src/modules/client/client.router.js
@@ -1,31 +1,27 @@
 import express from "express";
-import customerRouter from"../customer/customer.router.js"
-import {deleteClient, gClientLogin, googleAuthCallback, updateClient} from "./client.controller.js";
-import {asyncHandler} from "../../utils/catchError.js";
-import {auth, roles} from "../../middleware/auth.js";
-
+import customerRouter from "../customer/customer.router.js";
+import {
+    deleteClient,
+    gClientLogin,
+    getClients,
+    googleAuthCallback,
+    updateClient,
+} from "./client.controller.js";
+import { asyncHandler } from "../../utils/catchError.js";
+import { auth, roles } from "../../middleware/auth.js";
 
 const router = express.Router();
 
-router.use('/:clientId/newCustomer',customerRouter);
+router.use("/:clientId/newCustomer", customerRouter);
 
-router.get('/oauth2callback',
-    asyncHandler(googleAuthCallback)
-)
-router.get('/gLogin',
-    asyncHandler(gClientLogin)
-)
+router.get("/oauth2callback", asyncHandler(googleAuthCallback));
 
-router.patch('/',
-    auth(roles.Client),
-    asyncHandler(updateClient)
-)
+router.get("/gLogin", asyncHandler(gClientLogin));
 
+router.get("/details", auth([roles.Admin]), asyncHandler(getClients));
 
-router.delete('/',
-    auth(roles.Client),
-    asyncHandler(deleteClient)
-)
+router.patch("/", auth(roles.Client), asyncHandler(updateClient));
 
+router.delete("/", auth(roles.Client), asyncHandler(deleteClient));
 
-export default router
+export default router;
